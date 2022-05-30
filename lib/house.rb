93bc8de0de3aca5737587edf417a1b1939f51a0e
Caleb Knight <knightca@oregonstate.edu>
2022-05-30 15:42:36 -0700

    Implementation of line 10 up and running

--- lib/house.rb
+++ lib/house.rb
@@ -20,10 +20,8 @@ class House
       "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     when 9
       "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    when 10
-      "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     else
-      "This is #{phrase(num)} the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+      "This is #{phrase(num)}the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     end
   end
 
@@ -33,7 +31,9 @@ class House
       "the horse and the hound and the horn that belonged to "
       lyric = "the horse and the hound and the horn that belonged to " + phrase(num - 1)
     elsif num == 11
-      lyric = "the farmer sowing his corn that kept" + phrase(num - 1)
+      lyric = "the farmer sowing his corn that kept " + phrase(num - 1)
+    elsif num == 10
+      lyric = "the rooster that crowed in the morn that woke " + phrase(num - 1)
     end
     return lyric
   end
