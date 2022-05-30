34a98dd56a51c0cbd9d3e7e5a6fc1a48090fae0e
Caleb Knight <knightca@oregonstate.edu>
2022-05-30 15:38:42 -0700

    Added implementation for when we are on verse 11

--- lib/house.rb
+++ lib/house.rb
@@ -22,18 +22,20 @@ class House
       "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     when 10
       "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    when 11
-      "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     else
-      "This is #{phrase(num)} the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+      "This is #{phrase(num)} the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     end
   end
 
   def phrase(num)
     lyric = ""
     if num == 12
-      "the horse and the hound and the horn that belonged to"
+      "the horse and the hound and the horn that belonged to "
+      lyric = "the horse and the hound and the horn that belonged to " + phrase(num - 1)
+    elsif num == 11
+      lyric = "the farmer sowing his corn that kept" + phrase(num - 1)
     end
+    return lyric
   end
 
   def recite
