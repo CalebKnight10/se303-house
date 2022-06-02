62e11f5a6c7c6b9d8dcc3187e7abbc8e103c249f
Caleb Knight <knightca@oregonstate.edu>
2022-06-02 16:56:37 -0700

    Remove commented out code

--- lib/house.rb
+++ lib/house.rb
@@ -1,29 +1,9 @@
 class House
 
-  # @@phrases = [
-  #   "the house that Jack built.", "the malt that lay in ", "the rat that ate ",
-  #   "the cat that killed ", "the dog that worried ", "the cow with the crumpled horn that tossed ",
-  #   "the maiden all forlorn that milked ", "the man all tattered and torn that kissed ",
-  #   "the priest all shaven and shorn that married ", "the rooster that crowed in the morn that woke ",
-  #   "the farmer sowing his corn that kept ", "the horse and the hound and the horn that belonged to "
-  # ]
-
   def line(num)
     lyrics = Lyrics.new(num)
     "This is #{lyrics.phrase}\n"
   end
-
-  # def phrase(num)
-
-  #   lyric = ""
-
-  #   for i in num.downto(1) 
-  #     lyric += @@phrases[i-1]
-  #   end
-
-  #   return lyric
-
-  # end
 
   def recite
     1.upto(12).map{|num| line(num)}.join("\n")
