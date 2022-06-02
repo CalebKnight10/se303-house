6776f51b6e7402e744543879f981aac4909bf3d0
Caleb Knight <knightca@oregonstate.edu>
2022-06-02 16:56:14 -0700

    Added a class Lyrics to hold all lyrics and phrases Then implemented a lyrics object and commented out old code to see if it would work

--- lib/house.rb
+++ lib/house.rb
@@ -1,5 +1,39 @@
 class House
 
+  # @@phrases = [
+  #   "the house that Jack built.", "the malt that lay in ", "the rat that ate ",
+  #   "the cat that killed ", "the dog that worried ", "the cow with the crumpled horn that tossed ",
+  #   "the maiden all forlorn that milked ", "the man all tattered and torn that kissed ",
+  #   "the priest all shaven and shorn that married ", "the rooster that crowed in the morn that woke ",
+  #   "the farmer sowing his corn that kept ", "the horse and the hound and the horn that belonged to "
+  # ]
+
+  def line(num)
+    lyrics = Lyrics.new(num)
+    "This is #{lyrics.phrase}\n"
+  end
+
+  # def phrase(num)
+
+  #   lyric = ""
+
+  #   for i in num.downto(1) 
+  #     lyric += @@phrases[i-1]
+  #   end
+
+  #   return lyric
+
+  # end
+
+  def recite
+    1.upto(12).map{|num| line(num)}.join("\n")
+  end
+end
+
+class Lyrics
+
+  attr_accessor :num
+
   @@phrases = [
     "the house that Jack built.", "the malt that lay in ", "the rat that ate ",
     "the cat that killed ", "the dog that worried ", "the cow with the crumpled horn that tossed ",
@@ -8,20 +42,20 @@ class House
     "the farmer sowing his corn that kept ", "the horse and the hound and the horn that belonged to "
   ]
 
-  def line(num)
-      "This is #{phrase(num)}\n"
+  def initialize(num)
+    @num = num
   end
 
-  def phrase(num)
+  def phrase
 
     lyric = ""
+
     for i in num.downto(1) 
       lyric += @@phrases[i-1]
     end
+
     return lyric
+
   end
 
-  def recite
-    1.upto(12).map{|num| line(num)}.join("\n")
-  end
 end
