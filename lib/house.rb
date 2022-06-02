4d87b5726f574763d7529940a795e8b3b27a8ff7
Caleb Knight <knightca@oregonstate.edu>
2022-06-02 16:44:25 -0700

    Removed all cases and put into simple loop

--- lib/house.rb
+++ lib/house.rb
@@ -15,34 +15,8 @@ class House
   def phrase(num)
 
     lyric = ""
-
-    case num
-
-    when 12
-      # "the horse and the hound and the horn that belonged to "
-      lyric = @@phrases[11] + phrase(num - 1)
-    when 11
-      lyric = @@phrases[10] + phrase(num - 1)
-    when 10
-      lyric = @@phrases[9] + phrase(num - 1)
-    when 9
-      lyric = @@phrases[8] + phrase(num - 1)
-    when 8
-      lyric = @@phrases[7] + phrase(num - 1)
-    when 7
-      lyric = @@phrases[6] + phrase(num - 1)
-    when 6
-      lyric = @@phrases[5] + phrase(num - 1)
-    when 5
-      lyric = @@phrases[4] + phrase(num - 1)
-    when 4
-      lyric = @@phrases[3] + phrase(num - 1)
-    when 3
-      lyric = @@phrases[2] + phrase(num - 1)
-    when 2
-      lyric = @@phrases[1] + phrase(num - 1)
-    when 1
-      lyric = @@phrases[0]
+    for i in num.downto(1) 
+      lyric += @@phrases[i-1]
     end
     return lyric
   end
